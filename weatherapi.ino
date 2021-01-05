7718ebe09a781d19c5a0b746ebecb6f72bdad876
samaypiduru <67844360+samaypiduru@users.noreply.github.com>
2021-01-05 14:45:54 -0500

    Add files via upload

--- weatherapi.ino
+++ weatherapi.ino
@@ -13,23 +13,27 @@ const char DHT_TYPE = DHT11;  // uses the dht11 sensor
 const int DHT_PIN = 17;
 DHT dht(DHT_PIN, DHT_TYPE);
 
-WebServer server(8080);
+WebServer server(3000);
 
-void connectToWiFi() {
+void beginWiFi() {
+  WiFi.begin(SSID, PWD);
+
   Serial.print("Connecting to ");
   Serial.print(SSID);
   Serial.println(".....");
-  
-  WiFi.begin(SSID, PWD);
 
   while(WiFi.status() != WL_CONNECTED) {
-    delay(200);
+    if(WiFi.status() == WL_CONNECT_FAILED || WiFi.status() == WL_NO_SSID_AVAIL) {
+      Serial.println("Connection Failed");
+      return;
+    }
+    delay(100);
   }
   Serial.print("Connected to ");
   Serial.println(WiFi.localIP());
 }
 
-void api_route() {  // returns the appropriate metric for a request
+void apiReqListen() {  // returns the appropriate json object for a request
   server.on("/temperature", getTemp);
   server.on("/humidity", getHumidity);
   server.on("/heatinfo", getHeatInfo);
@@ -37,48 +41,48 @@ void api_route() {  // returns the appropriate metric for a request
 }
 
 void getTemp() {  // gets temp from dht11
-  Serial.println("Getting Temperature");
   newJsonObj("temperature", dht.readTemperature(true), "F");
+  Serial.println("Requested Temperature");
   server.send(200, "application/json", buffer);
 }
 
 void getHumidity() {  // gets humidity from dht11
-  Serial.println("Getting Humidity");
   newJsonObj("humidity", dht.readHumidity(), "%");
+  Serial.println("Requested Humidity");
   server.send(200, "application/json", buffer);
 }
 
 void getHeatInfo() {  // gets humidity and temperature from dht11
-  Serial.println("Getting Heat Info");
   jsonDocument.clear();
   addJsonObj("temperature", dht.readTemperature(true), "F");
   addJsonObj("humidity", dht.readHumidity(), "%");
   serializeJson(jsonDocument, buffer);
+  Serial.println("Requested Heat Info");
   server.send(200, "application/json", buffer);
 }
 
-void newJsonObj(char *metric, float value, char *unit) {
+void newJsonObj(char *metricName, float measurement, char *unit) {
   jsonDocument.clear();
-  jsonDocument["metric"] = metric;
-  jsonDocument["value"] = value;
+  jsonDocument["metric"] = metricName;
+  jsonDocument["value"] = measurement;
   jsonDocument["unit"] = unit;
   serializeJson(jsonDocument, buffer);
 }
 
-void addJsonObj(char *metric, float value, char *unit) {
-  JsonObject obj = jsonDocument.createNestedObject();
-  obj["metric"] = metric;
-  obj["value"] = value;
-  obj["unit"] = unit;
-}
-
-void setup() {
-  Serial.begin(115200);
-  dht.begin();
-  connectToWiFi();
-  api_route();
+void addJsonObj(char *metricName, float measurement, char *unit) {
+  JsonObject objData = jsonDocument.createNestedObject();
+  objData["metric"] = metricName;
+  objData["value"] = measurement;
+  objData["unit"] = unit;
 }
 
 void loop() {
   server.handleClient();
+}
+
+void setup() {
+  Serial.begin(115200);
+  beginWiFi();
+  dht.begin();
+  apiReqListen();
 }
